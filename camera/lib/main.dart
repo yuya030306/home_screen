db73101b64debeefa1cfe690f25cc7ebbfb10bfe
kazuibu <61813438+kazuibu@users.noreply.github.com>
2024-07-02 14:16:25 +0900

    2024/07/02 14:16

--- camera/lib/main.dart
+++ camera/lib/main.dart
@@ -217,9 +217,7 @@ class FullScreenImageScreen extends StatelessWidget {
 }
 
 class _CalendarScreenState extends State<CalendarScreen> {
-  //カレンダーのUIを作る
   DateTime _selectedDay = DateTime.now();
-  //現在選択されている日付を保持
   List<String> _imageUrls = [];
   final Map<DateTime, List<String>> _events = {};
   CalendarFormat _calendarFormat = CalendarFormat.month;
@@ -227,19 +225,16 @@ class _CalendarScreenState extends State<CalendarScreen> {
 
   @override
   void initState() {
-    //画面が初期化されたとき，画像を取得
     super.initState();
     _fetchImages();
   }
 
   Future<void> _fetchImages() async {
-    //Firebase Storageから画像のURLを取得(非同期)
     FirebaseStorage storage = FirebaseStorage.instanceFor(bucket: 'gs://camera-ea94f.appspot.com');
     final ListResult result = await storage.ref('downloads').listAll();
     final List<String> urls = [];
     for (var ref in result.items) {
       final String url = await ref.getDownloadURL();
-      // 各ファイルのダウンロードURLを取得する
       urls.add(url);
     }
     setState(() {
@@ -248,7 +243,6 @@ class _CalendarScreenState extends State<CalendarScreen> {
   }
 
   List<String> _getImagesForDay(DateTime day) {
-    //指定した日付に対応する画像のURLをリストで返す
     final String formattedDay = DateFormat('yyyyMMdd').format(day);
     return _imageUrls.where((url) => url.contains(formattedDay)).toList();
   }
@@ -256,14 +250,12 @@ class _CalendarScreenState extends State<CalendarScreen> {
   @override
   Widget build(BuildContext context) {
     final imagesForSelectedDay = _getImagesForDay(_selectedDay);
-    // 選択された日付に対応する画像のURLリストを取得する
     return Scaffold(
       appBar: AppBar(title: Text('Calendar')),
       backgroundColor: Color.fromARGB(255, 255, 255, 255),
       body: Column(
         children: [
           TableCalendar(
-            // TableCalendarウィジェットを使用してカレンダーを表示する
             focusedDay: _selectedDay,
             firstDay: DateTime(2000),
             lastDay: DateTime(2100),
@@ -274,7 +266,6 @@ class _CalendarScreenState extends State<CalendarScreen> {
               setState(() {
                 _selectedDay = selectedDay;
                 _currentImageIndex = 0;
-                // 日付が選択されたときに選択日付を更新
               });
             },
             headerStyle: HeaderStyle(
@@ -306,11 +297,38 @@ class _CalendarScreenState extends State<CalendarScreen> {
               selectedDecoration: BoxDecoration(
                 shape: BoxShape.circle,
                 border: Border.all(
-                  color: Color.fromARGB(255, 0, 0, 0), // 枠線の色を指定
-                  width: 2.0, // 枠線の太さを指定
+                  color: Color.fromARGB(255, 0, 0, 0),
+                  width: 2.0,
                 ),
               ),
-              defaultTextStyle: TextStyle(color: Colors.black), // カレンダーの日付の文字色を黒に設定
+              defaultTextStyle: TextStyle(color: Colors.black),
+            ),
+            calendarBuilders: CalendarBuilders(
+              defaultBuilder: (context, day, focusedDay) {
+                final imagesForDay = _getImagesForDay(day);
+                if (imagesForDay.isNotEmpty) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(imagesForDay.first),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        day.day.toString(),
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          backgroundColor: Colors.black54,
+                        ),
+                      ),
+                    ),
+                  );
+                } else {
+                  return Center(child: Text(day.day.toString()));
+                }
+              },
             ),
           ),
           if (imagesForSelectedDay.isEmpty)
@@ -330,33 +348,34 @@ class _CalendarScreenState extends State<CalendarScreen> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     ElevatedButton(
-                      onPressed: _currentImageIndex > 0 ? (){
-                        setState(() {
-                          _currentImageIndex = (_currentImageIndex - 1 + imagesForSelectedDay.length) % imagesForSelectedDay.length;
-                        });
-                      }:null,
+                      onPressed: _currentImageIndex > 0
+                          ? () {
+                              setState(() {
+                                _currentImageIndex = (_currentImageIndex - 1 + imagesForSelectedDay.length) % imagesForSelectedDay.length;
+                              });
+                            }
+                          : null,
                       child: Icon(Icons.arrow_back),
                       style: ElevatedButton.styleFrom(
                         padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
                         textStyle: TextStyle(fontSize: 18),
                       ),
                     ),
-
                     SizedBox(width: 30),
-
                     ElevatedButton(
-                      onPressed: _currentImageIndex < imagesForSelectedDay.length - 1 ? (){
-                        setState(() {
-                          _currentImageIndex = (_currentImageIndex + 1) % imagesForSelectedDay.length;
-                        });
-                      }:null,
+                      onPressed: _currentImageIndex < imagesForSelectedDay.length - 1
+                          ? () {
+                              setState(() {
+                                _currentImageIndex = (_currentImageIndex + 1) % imagesForSelectedDay.length;
+                              });
+                            }
+                          : null,
                       child: Icon(Icons.arrow_forward),
                       style: ElevatedButton.styleFrom(
                         padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
                         textStyle: TextStyle(fontSize: 18),
                       ),
                     ),
-
                   ],
                 ),
               ],
@@ -367,7 +386,6 @@ class _CalendarScreenState extends State<CalendarScreen> {
   }
 
   void _onFormatChanged(CalendarFormat format) {
-    //表示する期間を変える
     setState(() {
       _calendarFormat = format;
     });

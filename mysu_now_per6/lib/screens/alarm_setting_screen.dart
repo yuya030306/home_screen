051811b39620f79cbbbf9160404f4d1f7171951e
ryou2307 <ryou.tr2307@icloud.com>
2024-07-14 21:52:43 +0900

    アラームとグラフのUI修正

--- mysu_now_per6/lib/screens/alarm_setting_screen.dart
+++ mysu_now_per6/lib/screens/alarm_setting_screen.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'alarm_manager.dart';
-import 'goals/dashboard_screen.dart';
-
+import 'goals/goal_screen.dart';
 
 class AlarmPage extends StatefulWidget {
   static final GlobalKey<NavigatorState> navigatorKey =
-  GlobalKey<NavigatorState>();
+      GlobalKey<NavigatorState>();
 
   @override
   _AlarmPageState createState() => _AlarmPageState();
@@ -38,38 +36,92 @@ class _AlarmPageState extends State<AlarmPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('アラーム画面'),
+        backgroundColor: Colors.orange,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              _alarmTimeString ?? 'アラームをセットしてください',
-              style: TextStyle(fontSize: 24),
+      body: CustomPaint(
+        painter: BackgroundPainter(),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.orange.shade200, width: 2),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Text(
+                    _alarmTimeString ?? 'アラームをセットしてください',
+                    style: TextStyle(
+                      fontSize: 28,
+                      color: Colors.black54,
+                      fontFamily: 'Digital-7', // デジタル時計風フォント
+                      letterSpacing: 2.0,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: _pickTime,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.orange,
+                    foregroundColor: Colors.white,
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                    textStyle: TextStyle(fontSize: 20),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  child: Text(
+                    _alarmTimeString == null ? 'アラームをセット' : 'アラームを編集',
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    final alarmManager =
+                        Provider.of<AlarmManager>(context, listen: false);
+                    alarmManager.stopAlarm();
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => GoalScreen()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.orange,
+                    foregroundColor: Colors.white,
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                    textStyle: TextStyle(fontSize: 20),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  child: Text('アラームを停止'),
+                ),
+              ],
             ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _pickTime,
-              child: Text(_alarmTimeString == null ? 'アラームをセット' : 'アラームを編集'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                final alarmManager = Provider.of<AlarmManager>(context, listen: false);
-                alarmManager.stopAlarm();
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => DashboardScreen()),
-                );
-              },
-              child: Text('アラームを停止'),
-            ),
-          ],
+          ),
         ),
       ),
     );
   }
 
   Future<void> _pickTime() async {
+    final alarmManager = Provider.of<AlarmManager>(context, listen: false);
+    TimeOfDay initialTime = _selectedTime ?? TimeOfDay(hour: 0, minute: 0);
+    if (alarmManager.alarmTimeString != null) {
+      final alarmTime =
+          DateFormat('a h:mm').parse(alarmManager.alarmTimeString!);
+      initialTime = TimeOfDay(hour: alarmTime.hour, minute: alarmTime.minute);
+    }
+
+    final hourController =
+        FixedExtentScrollController(initialItem: initialTime.hour);
+    final minuteController =
+        FixedExtentScrollController(initialItem: initialTime.minute);
+
     showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -77,7 +129,7 @@ class _AlarmPageState extends State<AlarmPage> {
           contentPadding: EdgeInsets.zero,
           content: Container(
             width: 250,
-            height: 250,
+            height: 300,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -91,6 +143,7 @@ class _AlarmPageState extends State<AlarmPage> {
                           alignment: Alignment.center,
                           children: [
                             ListWheelScrollView.useDelegate(
+                              controller: hourController,
                               itemExtent: 50,
                               diameterRatio: 1.5,
                               physics: FixedExtentScrollPhysics(),
@@ -137,6 +190,7 @@ class _AlarmPageState extends State<AlarmPage> {
                           alignment: Alignment.center,
                           children: [
                             ListWheelScrollView.useDelegate(
+                              controller: minuteController,
                               itemExtent: 50,
                               diameterRatio: 1.5,
                               physics: FixedExtentScrollPhysics(),
@@ -182,11 +236,19 @@ class _AlarmPageState extends State<AlarmPage> {
                 ElevatedButton(
                   onPressed: () {
                     final alarmManager =
-                    Provider.of<AlarmManager>(context, listen: false);
+                        Provider.of<AlarmManager>(context, listen: false);
                     alarmManager.setAlarm(
                         _selectedHour, _selectedMinute, _alarmTimeString!);
                     Navigator.of(context).pop();
                   },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.orange,
+                    foregroundColor: Colors.white,
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
                   child: Text('OK'),
                 ),
               ],
@@ -200,7 +262,46 @@ class _AlarmPageState extends State<AlarmPage> {
   String _formatTime(TimeOfDay time) {
     final now = DateTime.now();
     final dt = DateTime(now.year, now.month, now.day, time.hour, time.minute);
-    final format = DateFormat.jm();
-    return format.format(dt);
+    final format = DateFormat('a h:mm'); // AM/PMの形式
+    return format
+        .format(dt)
+        .replaceFirst(' ', ' ')
+        .toUpperCase(); // AM/PM表記のカスタマイズ
+  }
+}
+
+class BackgroundPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.orange.shade100
+      ..style = PaintingStyle.fill;
+
+    final path = Path();
+    path.moveTo(0, size.height * 0.3);
+    path.quadraticBezierTo(
+        size.width * 0.5, size.height * 0.4, size.width, size.height * 0.3);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.close();
+
+    canvas.drawPath(path, paint);
+
+    paint.color = Colors.orange.shade200;
+
+    path.reset();
+    path.moveTo(0, size.height * 0.5);
+    path.quadraticBezierTo(
+        size.width * 0.5, size.height * 0.6, size.width, size.height * 0.5);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.close();
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
   }
 }
